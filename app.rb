e1965b46f016ce6349524c64e7a3803e1f439ecb
Qianqian Hu <qianhu@pindous.com>
2017-12-01 15:16:44 -0800

    revised form type

--- app.rb
+++ app.rb
@@ -3,16 +3,16 @@ require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word_definer')
 
-get('/') do
+get('/try') do
   erb(:input)
 end
 
 get('/output') do
   @word1 = params.fetch("word1")
   @word2 = params.fetch("word2")
-  word_definer =
+  word_definer =  WordDefiner.new(@word1, @word2)
 
-  if word1 == word2
+  if word1_definition?
     @definition = "word definition1."
   else
     @definition = "word definition2."
